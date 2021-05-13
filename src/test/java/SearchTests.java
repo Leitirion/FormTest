00e9b8dfa68905f327743e3af6fc54c305baa5ba
svasenkov <aanher@gmail.com>
2021-05-13 19:09:48 +0300

    added search test

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTests {
+
+    @Test
+    void selenideSearchTest() {
+        open("https://www.google.com/");
+
+        $(byName("q")).setValue("selenide").pressEnter();
+
+        $("#search").shouldHave(text("selenide.org"));
+    }
+}
